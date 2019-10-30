0210213a4a24625a6ea8c89474c838104db2951a
Ian Vo <ianvo@Ians-MacBook-Air.local>
2019-10-30 12:07:41 -0700

    better success messaging for clarity

--- app/src/main/java/com/example/omletlogin/MainActivity.java
+++ app/src/main/java/com/example/omletlogin/MainActivity.java
@@ -15,31 +15,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_GET_OMLET_USER_TOKEN = 123;
 
-    Button loginButton, copyButton;
-    private TextView omletUserTokenTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loginButton = findViewById(R.id.login);
-        copyButton = findViewById(R.id.copy);
-        copyButton.setVisibility(View.GONE);
-        omletUserTokenTextView = findViewById(R.id.omlet_user_token);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent getOmletUserTokenIntent = new Intent(MainActivity.this, WebLoginActivity.class);
                 startActivityForResult(getOmletUserTokenIntent, REQUEST_GET_OMLET_USER_TOKEN);
             }
         });
-        copyButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.copy).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("Omlet User Token", omletUserTokenTextView.getText());
+                ClipData clip = ClipData.newPlainText("Omlet User Token", ((TextView)findViewById(R.id.omlet_user_token)).getText());
                 clipboard.setPrimaryClip(clip);
                 Toast.makeText(MainActivity.this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
             }
@@ -50,8 +41,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK && requestCode == REQUEST_GET_OMLET_USER_TOKEN) {
-            omletUserTokenTextView.setText(data.getStringExtra(WebLoginActivity.EXTRA_OMLET_USER_TOKEN));
-            copyButton.setVisibility(View.VISIBLE);
+            ((TextView)findViewById(R.id.omlet_user_token)).setText(data.getStringExtra(WebLoginActivity.EXTRA_OMLET_USER_TOKEN));
+            findViewById(R.id.success_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.omlet_user_token).setVisibility(View.VISIBLE);
+            findViewById(R.id.copy).setVisibility(View.VISIBLE);
         }
     }
 }
